49378de01ad901c03428570a36b22d7e42c41a27
Jeff Blake <blakejef@oregonstate.edu>
2020-10-22 15:53:22 -0700

    Added main.c

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+	Written by: Jeff Blake
+	Date: 9/27/202
+	Info:
+	
+*/
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+
+
+int main()
+{
+	printf("Hello World\n");
+	return 0;
+}
+
